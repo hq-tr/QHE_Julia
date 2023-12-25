410f4a99daa92b2db39adfa9cfeb243a4f8a821c
hq-tr <hqt210@gmail.com>
2023-12-25 17:32:09 +0800

    Working on Bilayer lilbrary

--- FQH_bilayer.jl
+++ FQH_bilayer.jl
@@ -19,8 +19,10 @@ using .MiscRoutine
 using Plots
 using SpecialFunctions
 using BenchmarkTools
+using SparseArrays
 
 import Base.display
+import Base.split
 
 abstract type Abstractbilayer_state end
 
@@ -40,43 +42,69 @@ mutable struct bilayer_state_mutable<: Abstractbilayer_state
     #shift = zeros(LLindex) 
 end
 
+function getdim(vec::Abstractbilayer_state)
+    return lenght(vec.basis[1])
+end
+
 function split(vec::Abstractbilayer_state)
     state1 = disk_normalize(FQH_state(map(x->x[1],vec.basis), vec.coef))
     state2 = disk_normalize(FQH_state(map(x->x[2],vec.basis), vec.coef),0.5)
     return state1, state2
 end
 
-function printwf(vec::Abstractbilayer_state;fname="")
-    combined_basis = map(x->vcat(x[1], x[2]), vec.basis)
-    state = FQH_state(combined_basis, vec.coef)
-    D = dim(state)
-    if length(fname) == 0
-        println(D)
-        for i in 1:D
-            println(prod(string.((Int.(state.basis[i])))))
-            println(replace("$(state.coef[i])", "im"=>"j", " "=>""))
-        end
-    else
-        open(fname,"w") do f
-            write(f,"$D\n")
-            for i in 1:D
-                writebasis = prod(string.((Int.(state.basis[i]))))
-                writecoef  = replace("$(state.coef[i])", "im"=>"j", " "=>"")
-                write(f,"$writebasis\n$writecoef\n")
-            end 
-        end
-
-    end
+function basissplit(basis::BitVector)
+    No = length(basis) ÷ 2
+    return [basis[1:No], basis[No+1:end]]
 end
 
-single_particle_state_disk(z::Number,m::Integer,Δ::Number) = z.^(m+Δ) * exp.(-abs.(z)^2) * sqrt(2^m / (2π)) / sqrt(gamma(m+Δ))
+function basiscombine(basis::Vector{BitVector})
+    return vcat(basis[1], basis[2])
+end
 
-single_particle_state_disk(z::Vector{T} where T <: Number,m::Integer, Δ::Number) = z.^(m+Δ) * exp.(-abs.(z)^2) * sqrt(2^m / (2π)) / sqrt(gamma(m+Δ))
+function bilayerreadwf(fname::String; mutable=false)
+        f = open(fname)
+        content = readlines(f)
+        dim = parse(Int64, content[1])
+        basis_line = [BitVector(map(x->parse(Bool, x), split(y,""))) for y in content[2:2:end]]
+        basis = Vector{BitVector}[]
+        push!(basis, [basissplit(vec)[1] for vec in basis_line])
+        push!(basis, [basissplit(vec)[2] for vec in basis_line])
+        println("The dimension is $dim")
+        try
+            global co = [parse(Float64, x) for x in content[3:2:end]]
+        catch ArgumentError
+            println("Reading coefficients as complex numbers")
+            global co = [parse(Complex{Float64}, x) for x in content[3:2:end]]
+        finally
+            close(f)
+            #println("Success")
+        end
+        if mutable
+            return bilayer_state_mutable(basis,co)
+        else
+            return bilayer_state(basis, co)
+        end
+end
 
+function display(vec::Abstractbilayer_state)  bilayerprintwf(vec) end
+
+function bilayerprintwf(vec::Abstractbilayer_state;fname="",format=:BIN)
+    dim = length(vec.basis[1])
+    combined_basis = [vcat(vec.basis[1][i], vec.basis[2][i]) for i in 1:dim]
+    state = FQH_state(combined_basis, vec.coef)
+    printwf(state;fname=fname,format=format)
+end
+
+# Single-particle LLL eigenstates. Here Δ is the shift. 
+single_particle_state_disk(z::Number,m::Integer,Δ::Number) = z.^(m+Δ) * exp.(-abs.(z)^2) * sqrt(2^m / (2π)) / sqrt(gamma(m+Δ+1))
+
+single_particle_state_disk(z::Vector{T} where T <: Number,m::Integer, Δ::Number) = z.^(m+Δ) * exp.(-abs.(z)^2) * sqrt(2^m / (2π)) / sqrt(gamma(m+Δ+1))
+
+# Calculate the electron density given a state
 function disk_density(vec::Abstractbilayer_state)
     state1, state2 = split(vec)
     # Prepare the plot area
-    No = length(state1.basis[1])
+    No = length(state1.basis[1][1])
     R_max = sqrt(2*No)+0.2
     x = -R_max:0.05:R_max
     y = -R_max:0.05:R_max
@@ -128,5 +156,74 @@ function disk_density(vec::Abstractbilayer_state,fname::String)
     savefig(p, "$(fname).svg")
 end
 
-export Abstractbilayer_state, bilayer_state, bilayer_state_mutable, disk_density, printwf, display
+
+# Calculate the matrix representation of the density operator given a basis
+# Each λ and μ is a BitVector of length 2Nₒ where Nₒ is the number of orbital on each level. Functions basiscombine and basissplit may be useful.
+
+function density_matrix_element!(mat::SparseMatrixCSC{ComplexF64, Int64},  i::Int, j::Int, λ::BitVector,μ::BitVector, single_particle_function::Vector{ComplexF64})
+    Nₒ = length(λ[1]) ÷ 2 # Number of orbitals on each level.
+    # Easiest way is to conver the bilayer basis into a single level (of length 2Nₒ) and use the single-layer code.
+    check_difference = λ .⊻ μ
+    count_difference = count(check_difference)
+    if count_difference == 2
+        λ_a = bin2dex(check_difference.*λ)[1]
+        μ_b  = bin2dex(check_difference.*μ)[1]
+        a = count(λ[1:λ_a])
+        b = count(μ[1:μ_b])
+        if abs(λ_a-μ_b)<Nₒ # The two different elements must be in the same layer.    
+            term = (-1)^(a+b) * conj.(single_particle_function[λ_a+1]) .* single_particle_function[μ_b+1]
+            mat[i,j] += term
+            mat[j,i] += conj(term)
+        end
+    elseif count_difference == 0
+        #println(bin2dex(λ))
+        mat[i,j] += sum([abs2.(single_particle_function[m+1]) for m in bin2dex(λ)])
+    end   
+end
+
+function bilayer_density_element!(Nₒ::Int, den::Matrix{Float64}, coef::Number, λ::BitVector,μ::BitVector, single_particle_function::Vector{Matrix{ComplexF64}})
+    #Nₒ = length(λ[1]) ÷ 2 # Number of orbitals on each level.
+    check_difference = λ.⊻ μ
+    count_difference = count(check_difference)
+    if count_difference == 2
+        λ_a = bin2dex(check_difference.*λ)[1]
+        μ_b  = bin2dex(check_difference.*μ)[1]
+        a = count(λ[1:λ_a])
+        b = count(μ[1:μ_b])
+        #print("$(λ_a)\t$(μ_b)\t$a\t$b\t")
+        if abs(λ_a-μ_b)<Nₒ # The two different elements must be in the same layer.    
+            #print("Updating density.")
+            den .+= real.(coef * (-1)^(a+b) * conj.(single_particle_function[λ_a+1]) .* single_particle_function[μ_b+1])
+        end
+    elseif count_difference == 0
+        #println(bin2dex(Lam))
+       # print("Updating density.")
+        den .+= real.(coef*sum([abs2.(single_particle_function[m+1]) for m in bin2dex(λ)]))
+    end    
+    #println()
+end
+
+function bilayer_density_matrix(basis_list::Vector{BitVector}, single_particle_function::Vector{ComplexF64})
+    dim = length(basis_list)
+    mat = spzeros(ComplexF64, (dim,dim))
+    for i in 1:dim
+        for j in i:dim
+            density_matrix_element!(mat, i,j, basis_list[i], basis_list[j], single_particle_function)
+        end
+    end
+    return mat
+end
+
+function bilayer_density_matrix(basis_list::Vector{BitVector}, ϵ::Number, z_0::ComplexF64)
+    No = length(basis_list[1]) ÷ 2
+    ϕ = single_particle_state_disk # Alias. Use ϕ(z, m ,Δ)
+    single_particle_function= [ϕ.(z_0, m-No*(m>=No), ϵ*(m>=No)) for m in 0:2No]
+    mat = bilayer_density_matrix(basis_list, single_particle_function)
+    return mat
+end
+
+export Abstractbilayer_state, single_particle_state_disk,bilayer_state, 
+        bilayer_state_mutable, disk_density, bilayerprintwf, 
+        bilayerreadwf,display,bilayer_density_matrix,basissplit, 
+        basiscombine, bilayer_density_element!
 end # End of module
